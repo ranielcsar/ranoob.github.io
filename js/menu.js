8ee8d035c3888c94cc9032e7389ce8a262bf3fc5
Raniel <whyfck@gmail.com>
2019-07-29 10:05:18 -0300

    menu :D

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,7 @@
 $(document).ready( function() {
 
-   $('.botao-menu').click( function(e) {
-      var el = $(this);
-
-      $('.navbar-itens').toggleClass('navbar-mostrar');
-
-      $('.navbar-itens').toggle(200);
-
-      $('.navbar-itens-direita').css('display', 'flex');
+   $('.btnMenu').click( function() {
+      $('.cabecalho a').toggleClass('aberto');
    });
 
 });
