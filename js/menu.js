5babc7d9d37b55486ef96b852d96ace2d5b7170a
Raniel <whyfck@gmail.com>
2019-09-11 17:11:09 -0300

    mudança total :D

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
-$(document).ready( function() {
+window.onscroll = function() { scrollFunction() };
 
-   $('.btnMenu').click( function() {
-      $('.links a').toggleClass('aberto');
-   });
+function scrollFunction() {
 
-});
+   if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) 
+   {
+      document.querySelector(".cabecalho").style.padding = "1.25em";      
+      document.querySelector(".cabecalho").style.backgroundColor = "#24252A";
+      document.querySelector(".titulo-site").style.fontSize = "2.25em";
+      document.querySelector(".menu-link").style.marginTop = "1.9375rem";
+   } else {
+      document.querySelector(".cabecalho").style.padding = "2.6em";      
+      document.querySelector(".cabecalho").style.backgroundColor = "transparent";
+      document.querySelector(".titulo-site").style.fontSize = "2.7em";
+      document.querySelector(".menu-link").style.marginTop = "2.19rem";
+   }
+}
