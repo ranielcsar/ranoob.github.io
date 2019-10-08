4efd448fb14ac337dc173d7da645d1f8370bdb99
Raniel <whyfck@gmail.com>
2019-10-08 00:52:32 -0300

    mudanças :D

--- js/menu.js
+++ js/menu.js
@@ -5,12 +5,12 @@ function scrollFunction() {
    if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) 
    {
       document.querySelector(".cabecalho").style.padding = "1.25em";      
-      document.querySelector(".cabecalho").style.backgroundColor = "#24252A";
+      document.querySelector(".cabecalho").style.background = "linear-gradient(to bottom right, #24252A, #333333)";
       document.querySelector(".titulo-site").style.fontSize = "2.25em";
       document.querySelector(".menu-link").style.marginTop = "1.9375rem";
    } else {
       document.querySelector(".cabecalho").style.padding = "2.6em";      
-      document.querySelector(".cabecalho").style.backgroundColor = "transparent";
+      document.querySelector(".cabecalho").style.background = "transparent";
       document.querySelector(".titulo-site").style.fontSize = "2.7em";
       document.querySelector(".menu-link").style.marginTop = "2.19rem";
    }
