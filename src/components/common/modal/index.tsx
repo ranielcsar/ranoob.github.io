5d6f2b3e4f2c145a395c4ba2cdf288968d5fe42a
ranoob <whyfck@gmail.com>
2023-10-10 11:49:41 -0300

    fix: up the z-index to 50

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -11,7 +11,7 @@ type ModalProps = {
 export function Modal({ children, isOpen, onClose, title }: ModalProps) {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
