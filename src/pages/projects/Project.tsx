3aabe08d9ff3b39d9c6cb7313b865fd28d2afbe4
ranoob <whyfck@gmail.com>
2023-10-12 09:31:59 -0300

    fix: css adjustements due to new carousel

--- src/pages/projects/Project.tsx
+++ src/pages/projects/Project.tsx
@@ -1,10 +1,9 @@
 import { CodeSandboxIcon, GithubIcon } from '@/assets/icons'
-import { useTransitionAnimation } from '@/hooks'
 
 export type ProjectProps = {
   title: string
   description: string
-  image: string | typeof import('*.webp')
+  image: string
   links: {
     sandbox: string
     github: string
@@ -12,58 +11,51 @@ export type ProjectProps = {
 }
 
 export function Project(project: ProjectProps) {
-  const [ref] = useTransitionAnimation({
-    from: {
-      opacity: 0,
-      y: -20,
-    },
-    to: {
-      opacity: 1,
-      y: 0,
-    },
-  })
-
   return (
-    <div className="flex h-[65vh] w-full rounded-sm text-white xl:m-auto xl:h-[82vh]" ref={ref}>
-      <section className="relative m-auto flex h-full flex-col gap-5 md:px-5 xl:p-0">
-        <header className="text-2xl font-bold xl:text-4xl">{project.title}</header>
+    <div className="mx-2 flex flex-col gap-5 md:h-[75vh] xl:h-full xl:gap-5 w-full xl:w-auto rounded-sm text-white">
+      <section className="relative m-auto flex h-full flex-col gap-5 xl:p-0">
+        <header className="text-2xl font-bold xl:text-4xl">
+          {project.title}
+        </header>
 
         <p className="text-lg">{project.description}</p>
 
-        <figure className="flex-[2] shadow-sm dark:shadow-none md:w-[95vw] lg:w-[68vw] xl:w-auto">
-          <img
-            className="h-full max-h-[40vh] w-full object-cover xl:max-h-[60vh] xl:object-fill"
-            src={project.image as string}
-            alt={`${project.title} image`}
-            rel="preload"
-            loading="eager"
-          />
-        </figure>
-
-        <footer className="flex flex-[0.5] items-center justify-end gap-4 md:flex-1">
-          <span className="text-4xl">{'['}</span>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={project.links.sandbox}
-            title="CodeSandbox"
-            className="h-10 w-10 transition-colors hover:text-primary"
-          >
-            <CodeSandboxIcon />
-          </a>
-          <span className="text-4xl">,</span>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={project.links.github}
-            title="Github"
-            className="h-10 w-10 transition-colors hover:text-primary"
-          >
-            <GithubIcon />
-          </a>
-          <span className="text-4xl">{']'}</span>
-        </footer>
+        <img
+          className="h-[45vh] md:h-full aspect-video w-full object-fill rounded-sm"
+          src={project.image}
+          alt={`${project.title} image`}
+          rel="preload"
+          loading="eager"
+        />
       </section>
+
+      <footer className="flex items-center justify-end gap-4 md:flex-1">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={project.links.sandbox}
+          title="CodeSandbox"
+          className="flex items-center rounded-sm bg-white w-max gap-1 p-2 text-black transition-colors hover:text-primary"
+        >
+          <i className="h-7 w-7">
+            <CodeSandboxIcon />
+          </i>
+          <span className="text-xl">Demo</span>
+        </a>
+
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={project.links.github}
+          title="Github"
+          className="flex items-center rounded-sm bg-white w-max gap-1 p-2 text-black transition-colors hover:text-primary"
+        >
+          <i className="h-7 w-7">
+            <GithubIcon />
+          </i>
+          <span className="text-xl">Code</span>
+        </a>
+      </footer>
     </div>
   )
 }
