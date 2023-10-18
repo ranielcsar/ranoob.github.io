f5769b4d08ab3e76293be5dc3a4707c0e63b516b
ranoob <whyfck@gmail.com>
2023-10-18 16:32:14 -0300

    refactor: changes due to carousel refactor

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -7,22 +7,20 @@ import {
   NLWSpacetimePrint,
   NextShoppingPrint,
 } from '@/assets/project-imgs'
-import { useState } from 'react'
 
 export function ProjectsPage() {
-  const [activeItem, setActiveItem] = useState(projects[0].title)
-
   return (
     <div className="mt-20 h-full w-full">
-      <Carousel setActiveItem={setActiveItem}>
-        {projects.map((project) => (
+      <Carousel
+        items={projects}
+        children={({ item: project, activeItem }) => (
           <Project
             key={project.title}
-            {...{ project }}
-            activeItem={activeItem === project.title}
+            project={project}
+            activeItem={activeItem}
           />
-        ))}
-      </Carousel>
+        )}
+      />
     </div>
   )
 }
