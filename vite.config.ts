ff30a15b1d3c43db19e684ddf050953af69ef788
ranoob <whyfck@gmail.com>
2023-10-25 16:16:02 -0300

    feat: code splitting with manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,37 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+
+import { dependencies } from './package.json'
+
+const exclVendors = ['react', 'react-router-dom', 'react-dom']
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {}
+  Object.keys(deps).forEach((key) => {
+    if (exclVendors.includes(key)) return
+    chunks[key] = [key]
+  })
+  return chunks
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   define: {
     'process.env': {},
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 })
