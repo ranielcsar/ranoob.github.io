96f96890a81436669c5e52380911ddc5e2d82798
ranoob <whyfck@gmail.com>
2023-10-27 18:57:01 -0300

    fix: remove backdrop-filter due to laggy on xl screen

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -15,30 +15,20 @@ export function Modal({
   title,
 }: PropsWithChildren<ModalProps>) {
   return (
-    <Transition appear show={isOpen} as={Fragment}>
+    <Transition show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-50" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-zinc-900 bg-opacity-60" />
-        </Transition.Child>
+        <div className="fixed inset-0 bg-black/60 w-full h-full" />
 
         <div className="fixed inset-0 overflow-y-auto">
           <div className="m-auto flex h-full w-[90%] items-center justify-center text-center md:p-4">
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
+              enter="ease-out duration-200"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
               leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
             >
               <Dialog.Panel className="relative w-full max-w-lg transform overflow-hidden rounded-md bg-white p-6 text-left align-middle shadow-xl transition-all md:h-[79vh] xl:h-auto">
                 <Dialog.Title
