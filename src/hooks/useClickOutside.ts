b7a0f124a68a09b50b16e6d87997a687501a7dbd
ranoob <whyfck@gmail.com>
2023-11-15 11:20:06 -0300

    feat: onClickOutside hook and close theme on click outside

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,17 @@
+import { RefObject, useEffect } from 'react'
+
+export const useClickOutside = (ref: RefObject<any>, callback: () => void) => {
+  const handleClick = (e: Event) => {
+    if (ref.current && !ref.current.contains(e.target)) {
+      callback()
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  })
+}
