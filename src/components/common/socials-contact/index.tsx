f74a4dcf21c732a1e6023f24f75f51764ef2b906
ranoob <whyfck@gmail.com>
2024-01-19 08:33:47 -0300

    refac: change linkedin url

--- src/components/common/socials-contact/index.tsx
+++ src/components/common/socials-contact/index.tsx
@@ -16,7 +16,7 @@ export function SocialLinksAndContact() {
 
       <a
         className="h-10 w-10 md:h-14 md:w-14 lg:w-10 lg:h-10 bg-primary shadow-neo shadow-secondary rounded-full text-secondary cursor-pointer transition-colors border-2 border-secondary p-2"
-        href="https://www.linkedin.com/in/raniel-cesar-5bb0991a3/"
+        href="https://www.linkedin.com/in/ranielcsar"
         referrerPolicy="no-referrer"
         target="_blank"
         aria-label="Acessar perfil no Linkedin"
