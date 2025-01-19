7701ce5628a12d411a8da25117e29db6581a05c7
ranoob <ranielcsardev@gmail.com>
2025-01-19 17:58:28 -0300

    fix: posição de elementos em :lg

--- src/components/layout/header/_index.tsx
+++ src/components/layout/header/_index.tsx
@@ -8,11 +8,11 @@ export function Header() {
   return (
     <header className="sticky top-0 bg-primary z-10 w-full h-max py-3 border-b-2 border-secondary">
       <div className="flex flex-col gap-4 lg:flex-row items-center justify-between max-w-5xl m-auto">
-        <div className="flex flex-1 order-2 gap-5 items-center">
+        <div className="flex flex-1 order-2 lg:order-1 gap-5 items-center">
           <SocialLinksAndContact />
         </div>
 
-        <strong className="order-1 tracking-wider xl:text-2xl m-auto flex-[2] text-center">
+        <strong className="order-1 lg:order-2 tracking-wider xl:text-2xl m-auto flex-[2] text-center">
           {'<'}
           ranielcsar
           {'/>'}
